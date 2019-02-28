e0057031ebf90e0346861c6c260265adbcaf2cd5
lzy_sidryu <ziyuliu@brandeis.edu>
2019-02-28 10:19:53 -0500

    Complete register and login routes. Not tested yet.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# NanoTwitter
+# NanoTwitter V0.2
 course project for Cosi105b
 
 ## Team Member
@@ -6,3 +6,18 @@ course project for Cosi105b
 * Yirun Zhou yirunzhou@brandeis.edu
 * Fengzhencheng Zeng fzeng@brandeis.edu
 
+## Documentations:
+* [Route](/doc/ROUTE.md)
+* [Schema](/doc/SCHEMADB.md)
+
+## Version Changelogs
+
+### NT0.1
+* Created github repo.
+* Designed basic UI, routes and schemadb.
+
+### NT0.2
+* Created all tables and migrations.
+* Created skeleton sinatra app and React app.
+* Completed Register/Login pages.
+* Heroko deployment experiment succeeded.
--- app.rb
+++ app.rb
@@ -13,6 +13,36 @@ require './models/hashtag.rb'
 require './models/tweets_hashtags_relationship.rb'
 require './models/notification.rb'
 
+enable :sessions  
+
+helpers do
+end
+
 get '/' do
   "Hello Sinatra!"
+end
+
+post '/register/?' do
+  @user = User.new(params[:user])
+  @user.password = params[:password]
+  @user.save!
+  if @user.valid?
+    redirect '/', 'register succeeded'
+  else
+    redirect '/register/', 'register failed'
+  end
+end
+
+post '/login/?' do
+  if (params[:username] =~ URI::MailTo::EMAIL_REGEXP) != nil
+    @user = User.find_by_email(params[:username])
+  else
+    @user = User.find_by_username(params[:username])
+  end
+  redirect '/login/' unless @user.valid? && @user.correct_password?(params[:password])
+  if @user.password == params[:password]
+    session[:user] = @user.user
+  else
+    redirect '/'
+  end
 end
