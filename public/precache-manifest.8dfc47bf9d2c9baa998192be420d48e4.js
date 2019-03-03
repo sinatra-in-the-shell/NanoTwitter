78591d7d2ea2c1031922d0c4e267d2878465b2ca
yirunzhou <yirunzhou@brandeis.edu>
2019-03-03 14:54:48 -0500

    test auto dep

---
+++ public/precache-manifest.8dfc47bf9d2c9baa998192be420d48e4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "2fc7e385890ae5def4f6",
+    "url": "/static/js/main.2fc7e385.chunk.js"
+  },
+  {
+    "revision": "69b6717e921c7a7c39c3",
+    "url": "/static/js/2.69b6717e.chunk.js"
+  },
+  {
+    "revision": "2fc7e385890ae5def4f6",
+    "url": "/static/css/main.ed15185c.chunk.css"
+  },
+  {
+    "revision": "3151956dca663346bc6f68fb7053be31",
+    "url": "/index.html"
+  }
+];
