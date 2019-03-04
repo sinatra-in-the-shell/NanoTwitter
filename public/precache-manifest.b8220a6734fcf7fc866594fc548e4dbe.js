1131d2abde45d39b7ccdabdb8657fc9acd30e428
yirunzhou <yirunzhou@brandeis.edu>
2019-03-04 17:18:33 -0500

    api doc

---
+++ public/precache-manifest.b8220a6734fcf7fc866594fc548e4dbe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "46398780429eb8068fb7",
+    "url": "/static/js/main.46398780.chunk.js"
+  },
+  {
+    "revision": "69b6717e921c7a7c39c3",
+    "url": "/static/js/2.69b6717e.chunk.js"
+  },
+  {
+    "revision": "46398780429eb8068fb7",
+    "url": "/static/css/main.b4446913.chunk.css"
+  },
+  {
+    "revision": "39dec10cf0e73dc14b5a79477edf3c56",
+    "url": "/index.html"
+  }
+];
