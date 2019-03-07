dc455e8215f03b958d31711c21996bfe94e252c0
yirunzhou <yirunzhou@brandeis.edu>
2019-03-06 22:38:16 -0500

    use create first.. comment out import

--- app.rb
+++ app.rb
@@ -71,7 +71,7 @@ end
 # POST /test/user/{u}/tweets?count=n
 post '/test/user/:userid/tweets' do
   print 'userid = ', params['userid'], ' count = ', params['count'], "\n"
-  import_tweets(params['userid'], params['count'])
+  import_tweets(params['userid'].to_i, params['count'].to_i)
 # {u} can be the user id of some user, or the keyword testuser
 # n is how many randomly generated tweets are submitted on that users behalf
 end
--- helpers/random_tweet_helper.rb
+++ helpers/random_tweet_helper.rb
@@ -1,13 +1,35 @@
 def import_tweets (userid, num_tweets)
-  tweets = []
-  num_tweets.times do |i|
-    # text = Faker::String.random
-    tweets << Tweet.create(:text => 'text test', 
+  # tweets = []
+  # (0..num_tweets).each do |i|
+  #   # text = 
+  #   tweets << Tweet.new(
+  #                       :text => Faker::String.random, 
+  #                       :tweet_type => 'orig',
+  #                       :user_id => userid,
+  #                       :comment_to_id => 0,
+  #                       :retweet_from_id => 0)
+  # end
+  # Tweet.import tweets
+
+  Tweet.create(
+                        :text => Faker::String.random, 
                         :tweet_type => 'orig',
                         :user_id => userid,
                         :comment_to_id => 0,
                         :retweet_from_id => 0)
-  end
-  Tweet.import tweets
 end
+
+
+# create_table "tweets", force: :cascade do |t|
+#   t.bigint "user_id"
+#   t.bigint "comment_to_id"
+#   t.bigint "retweet_from_id"
+#   t.text "text"
+#   t.string "tweet_type"
+#   t.datetime "created_at", null: false
+#   t.datetime "updated_at", null: false
+#   t.index ["comment_to_id"], name: "index_tweets_on_comment_to_id"
+#   t.index ["retweet_from_id"], name: "index_tweets_on_retweet_from_id"
+#   t.index ["user_id"], name: "index_tweets_on_user_id"
+# end
 
