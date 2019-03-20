214a861b4f7b6bbb3f518db75def5b3bb9ed2241
yirunzhou <yirunzhou@brandeis.edu>
2019-03-20 17:45:22 -0400

    add testing-token to pass login

--- app.rb
+++ app.rb
@@ -24,7 +24,8 @@ end
 before do
   pass if (%w[login register].include?(request.path_info.split('/').last)) \
            || request.path_info.include?('test') \
-           || request.path_info.include?('loaderio-b2296ad8f5d2ab4dfcc4ce34a0d36fa8')
+           || request.path_info.include?('loaderio-b2296ad8f5d2ab4dfcc4ce34a0d36fa8')\
+           || request.path_info.include?('testing-token=sits-testing')
   if not logged_in?
     halt 401, 'not logged_in'
   end
