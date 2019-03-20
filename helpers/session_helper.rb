335cb60af514dafa1cbeee23121d1b03c3e30b2c
lzy_sidryu <ziyuliu@brandeis.edu>
2019-03-20 14:34:54 -0400

    minor bug fix

--- helpers/session_helper.rb
+++ helpers/session_helper.rb
@@ -9,9 +9,9 @@ def remember(user)
 end
 
 def current_user
-  if (user_id == session[:user_id])
+  if user_id = session[:user_id]
     @current_user ||= User.find_by(id: user_id)
-  elsif (user_id == cookies[:user_id])
+  elsif user_id = cookies[:user_id]
     user = User.find_by(id: user_id)
     if user && user.authenticated?(cookies[:remember_token])
       log_in user
