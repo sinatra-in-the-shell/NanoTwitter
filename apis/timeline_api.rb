3583d2860223648fe0c43889a2ba8003a5f8efe2
yirunzhou <yirunzhou@brandeis.edu>
2019-03-24 15:02:25 -0400

    add their own tweets to timeline

--- apis/timeline_api.rb
+++ apis/timeline_api.rb
@@ -1,15 +1,15 @@
 get '/api/timeline' do
-  user_id = params['user_id']
+  user_id = params['user_id'].to_i
 
   @timeline = Tweet.find_by_sql(["
-    SELECT 
-      Tweets.*
+    SELECT Tweets.*
     FROM Tweets, Follows
     WHERE
       Follows.from_user_id = ? AND
-      Tweets.user_id = Follows.to_user_id
+      Tweets.user_id = Follows.to_user_id OR
+      Tweets.user_id = ?
     ORDER BY Tweets.updated_at DESC
-  ", user_id.to_i])
+  ", user_id, user_id])
 
   if @timeline
     json_response 200, @timeline.to_a
