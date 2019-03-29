6c4fd9af9b0b3549635c860d63e3577f65bb2d80
yirunzhou <yirunzhou@brandeis.edu>
2019-03-29 19:46:58 -0400

    resolve conflict  and remove user_id from params, only use current_user for consistency

--- apis/timeline_api.rb
+++ apis/timeline_api.rb
@@ -2,30 +2,6 @@ get '/api/timeline' do
   user = current_user
   limit = params['limit'] || 20
 
-<<<<<<< HEAD
-  @timeline = Tweet.find_by_sql(["
-    SELECT Tweets.*
-    FROM Tweets, Follows
-    WHERE
-      Follows.from_user_id = ? AND
-      Tweets.user_id = Follows.to_user_id OR
-      Tweets.user_id = ?
-    ORDER BY Tweets.updated_at DESC
-    LIMIT ?
-  ", user.id, user.id, limit])
-
-  if @timeline
-    json_response 200, @timeline.to_a
-  else
-    json_response 404, nil
-  end
-end
-
-get '/api/timeline/cached' do
-  user = current_user
-
-=======
->>>>>>> a04e0fc7bbf436a495b75166738872d3cfbaa5be
   redis_client = Redis.new(url: ENV['REDIS_URL'] || 'redis://localhost:6379')
   timeline = []
   cached = redis_client.lrange(user.id, 0, -1)
