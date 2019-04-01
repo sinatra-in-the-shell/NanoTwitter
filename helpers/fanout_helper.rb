6f13f25ffaebe7a47af4225be5d987c8cd5517a7
lzy_sidryu <ziyuliu@brandeis.edu>
2019-03-31 20:41:32 -0400

    merge

--- helpers/fanout_helper.rb
+++ helpers/fanout_helper.rb
@@ -1,6 +1,6 @@
 def fanout_helper(user, tweet)
   redis_client = Redis.new(url: ENV['REDIS_URL'] || 'redis://localhost:6379')
-  
+
   user_id = user.id
   tweet_id = tweet.id
   followers = user.followers
