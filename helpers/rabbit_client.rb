059ba168dd37c4f9ec6a2d194beaad8010d511cc
QuaternaryKFC <zeng.351@buckeyemail.osu.edu>
2019-04-19 18:55:39 -0700

    Added rabbit client

---
+++ helpers/rabbit_client.rb
@@ -0,0 +1,56 @@
+class RabitClient
+  attr_accessor :calls, :connection, :channel, :helper,
+                :server_queue_name, :reply_queue, :exchange
+
+  def initialize(rabbit_url, server_queue_name)
+    @connection = Bunny.new(url: rabbit_url)
+    @connection.start
+
+    @channel = connection.create_channel
+    @exchange = channel.default_exchange
+    @server_queue_name = server_queue_name
+
+    setup_reply_queue
+  end
+
+  def call(params, timeout=5)
+    call_id = generate_uuid
+
+    exchange.publish(
+      params.to_json,
+      routing_key: server_queue_name,
+      correlation_id: call_id,
+      reply_to: reply_queue.name
+    )
+    loop do
+      sleep 0.01
+      timeout -= 0.01
+      break if calls[call_id]
+      break if timeout <= 0
+    end
+
+    calls.delete call_id
+  end
+
+  def stop
+    channel.close
+    connection.close
+  end
+
+  private
+
+  def setup_reply_queue
+    @reply_queue = channel.queue('', exclusive: true)
+    @calls = {}
+
+    reply_queue.subscribe do |delivery_info, properties, payload|
+      response_id = properties[:correlation_id]
+      calls[response_id] = payload
+    end
+  end
+
+  def generate_uuid
+    # very naive but good enough for code examples
+    "#{Time.now.to_i}#{rand}#{rand}"
+  end
+end
