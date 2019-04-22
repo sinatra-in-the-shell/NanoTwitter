ce5a2daef15409f4a4616fa3bbe8951276163509
QuaternaryKFC <zeng.351@buckeyemail.osu.edu>
2019-04-22 13:15:43 -0700

    Add retweet_from to Tweet

--- models/tweet.rb
+++ models/tweet.rb
@@ -1,5 +1,6 @@
 class Tweet < ActiveRecord::Base
   belongs_to :user
+  belongs_to :retweet_from, class_name: 'Tweet', foreign_key: 'retweet_from_id'
 
   has_many :retweets, class_name: 'Tweet', foreign_key: 'retweet_from_id'
   has_many :comments, class_name: 'Tweet', foreign_key: 'comment_to_id'
