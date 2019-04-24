ca064a026a6a7acdb8f768de8659e2c52afc05a4
QuaternaryKFC <zeng.351@buckeyemail.osu.edu>
2019-04-24 13:10:24 -0700

    Added follow server

--- app.rb
+++ app.rb
@@ -25,7 +25,7 @@ require 'puma'
 Dir["./models/*.rb"].each {|file| require file }
 
 # set :server, "thin"
-configure { set :server, :puma }
+configure { set :server, :thin }
 
 enable :sessions
 
@@ -45,7 +45,14 @@ $followers_redis.clear
 $leaders_redis.clear
 $timeline_redis.clear
 
-$rabbit_client = RabbitClient.new(ENV['RABBITMQ_URL'], 'tweet_server')
+begin
+  follow_server
+  tweet_client
+rescue Interrupt => _
+  follow_server.stop
+  tweet_client.stop
+  pp "** rabbit interupted **"
+end
 
 before do
   pass if (%w[login register].include?(request.path_info.split('/').last)) \
@@ -53,7 +60,7 @@ before do
            || request.path_info.include?('loaderio-b2296ad8f5d2ab4dfcc4ce34a0d36fa8') \
            || params[:test_user]
   if not logged_in?
-    # if request.path_info.include?('api') 
+    # if request.path_info.include?('api')
     #   halt 401, {errors: 'not logged in'}.to_json
     # els
     if request.get?
