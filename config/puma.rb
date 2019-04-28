e22b2b2fb83d1d9943cef83d03e587a171d351f3
Ziyu Liu <ziyuliu@brandeis.edu>
2019-04-28 12:04:28 -0400

    preload app for puma

--- config/puma.rb
+++ config/puma.rb
@@ -2,12 +2,14 @@ workers Integer(ENV['WEB_CONCURRENCY'] || 2)
 threads_count = Integer(ENV['THREAD_COUNT'] || 10)
 threads threads_count, threads_count
 
+preload_app!
+
 rackup      DefaultRackup
 port        ENV['PORT']     || 3000
 environment ENV['RACK_ENV'] || 'development'
 
 on_worker_boot do
     ActiveSupport.on_load(:active_record) do
-      ActiveRecord::Base.establish_connection
+        ActiveRecord::Base.establish_connection
     end
-  end
+end
