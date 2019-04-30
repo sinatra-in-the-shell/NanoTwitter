9fe921d3172960932fdddcb29ab26139b7bf6815
QuaternaryKFC <zeng.351@buckeyemail.osu.edu>
2019-04-30 12:45:56 -0700

    Run build

---
+++ public/precache-manifest.2c3c429ea6ebf650c93179e58add8804.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "72c397e58759f988c1a1",
+    "url": "/static/js/main.a2cccc59.chunk.js"
+  },
+  {
+    "revision": "2df5bd18ea81cf9fb858",
+    "url": "/static/js/2.b9184191.chunk.js"
+  },
+  {
+    "revision": "ccdfa9fefc350016da4c2fd07bc0264e",
+    "url": "/index.html"
+  }
+];
