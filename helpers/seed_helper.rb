3e589da7f7cc75a5071ff1d25eb3ff8f840fa720
yirunzhou <yirunzhou@brandeis.edu>
2019-05-06 22:24:13 -0400

    change seed flush limit to be 500 to avoid hobby dev db crash

--- helpers/seed_helper.rb
+++ helpers/seed_helper.rb
@@ -68,8 +68,8 @@ def load_seed_tweets(count, filename)
       updated_at: row[2]
     )
     row_count += 1
-    # flush the array every 100 rows to limit memory usage
-    if (row_count % 100).zero?
+    # flush the array every 500 rows to limit memory usage
+    if (row_count % 500).zero?
       flush_tweets_into_database(tweets, columns)
     end
   end
